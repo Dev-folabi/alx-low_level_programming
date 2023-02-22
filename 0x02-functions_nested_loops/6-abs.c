4b41c2a44af513657a3b4489238d18c7d8745c5c
forlabi95 <yusufafolabi95@gmail.com>
2023-02-22 06:07:02 -0800

    loop

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _abs  - main function
+*
+* @a: parameter to be printed
+*
+* Return: always 0
+*/
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+		return (a);
+}
