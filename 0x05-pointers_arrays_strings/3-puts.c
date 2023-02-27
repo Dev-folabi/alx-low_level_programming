8da163fded3684c80db6507b74008aa4c3757b22
forlabi95 <yusufafolabi95@gmail.com>
2023-02-27 10:14:23 -0800

    c pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <unistd.h>
+/**
+* _puts - Prints a string to stdout, followed by a new line.
+*
+* @str: Pointer to the string to print.
+*
+* Return: void.
+*/
+
+void _puts(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	write(STDOUT_FILENO, str, len);
+	write(STDOUT_FILENO, "\n", 1);
+}
+
