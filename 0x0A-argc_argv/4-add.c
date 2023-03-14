47dcfd6a1380df04a311b915708edeb814871960
forlabi95 <yusufafolabi95@gmail.com>
2023-03-14 16:32:16 -0700

    arg

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - Entry point
+* @argc: Number of command line arguments
+* @argv: Array of command line arguments
+*
+* Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, j, num;
+
+	if (argc == 1)
+	{
+	printf("0\n");
+	return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+		if (!isdigit(argv[i][j]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	num = atoi(argv[i]);
+	if (num < 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum += num;
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
+
